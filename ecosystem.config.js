d953310243ed0039a03cf01adb62682a1d249157
iamlive <71422116+iamcrypto@users.noreply.github.com>
2025-04-09 08:44:22 +0530

    commit
    and push

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,30 @@
 module.exports = {
-    apps: [
-      {
-        name: "app",
-        script: "./server.js",
-        interpreter: "./node_modules/.bin/babel-node",
-        watch: true,
+  apps: [
+    {
+      name: "app",
+      script: "./server.js",
+      interpreter: "./node_modules/.bin/babel-node",
+      watch: true,
+      env_development: {
+        NODE_ENV: "development",
+        // Add other environment variables for development here
       },
-      {
-        name: "database",
-        script: "./src/modal/CreateDatabase.js",
-        interpreter: "./node_modules/.bin/babel-node",
-        watch: true,
+      env_production: {
+        NODE_ENV: "production",
+        // Add other environment variables for production here
       },
-    ],
-  };
-  
+    },
+    {
+      name: "database",
+      script: "./src/modal/CreateDatabase.js",
+      interpreter: "./node_modules/.bin/babel-node",
+      watch: true,
+      env_development: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
